5f27d65353ae35b4f2a99c27604f2ec8d41b6887
Andrés Pereira de Lucena <apereira@alabs.org>
2018-10-10 16:12:59 +0200

    Removes invitations link and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  match "/users/invitations", to: redirect("/404"), via: [:get, :post]
+  match "/users/invitations/*", to: redirect("/404"), via: [:get, :post]
+  match "/account/invitations", to: redirect("/404"), via: [:get, :post]
+  match "/account/invitations/*", to: redirect("/404"), via: [:get, :post]
+
   if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+	mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   mount Decidim::Core::Engine => '/'
