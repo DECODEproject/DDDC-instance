a2c82d4eea5ee66d33313a2b36aeaeb66b750bd8
Oriol Gual <oriol@codegram.com>
2018-10-11 17:23:27 +0200

    Update email config

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,7 +2,7 @@
 
 Decidim.configure do |config|
   config.application_name = "Decidim DECODE"
-  config.mailer_sender = "change-me@domain.org"
+  config.mailer_sender = "dddc@decodeproject.eu"
 
   # Change these lines to set your preferred locales
   config.default_locale = :en
