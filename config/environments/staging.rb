81a03aa4a2b884767149501b760460f8a0e544dc
Andrés Pereira de Lucena <apereira@alabs.org>
2018-10-16 16:40:39 +0200

    Disable public view with user and password

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -101,4 +101,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use Rack::Auth::Basic, "Beta Access" do |username, password|
+    'dddc' == password
+  end
 end
